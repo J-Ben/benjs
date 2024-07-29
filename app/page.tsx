2fe08351514274b10f6a490dbebd2d70d7afc605
Ben-J MK <ben-jamin@anjcprod.org>
2024-07-29 14:59:53 +0200

    feature: basis & test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <main className="container flex items-center justify-center mx-auto h-screen">
-      <Button>Bonjour la ville de Paris</Button>
+      <Button>Ben-J</Button>
     </main>
   );
 }
