487e4cb734896a0d8001edc57983b25a709fdd63
Ben-J MK <ben-jamin@anjcprod.org>
2024-07-29 12:39:59 +0200

    feature: basics

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main className="container flex items-center justify-center mx-auto h-screen">
+      <Button>Bonjour la ville de Paris</Button>
+    </main>
+  );
+}
